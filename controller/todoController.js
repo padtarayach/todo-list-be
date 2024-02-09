ccb9cce0e67f1e96285765ca13039d847f1ae39e
padtarayach <gibbbu@gmail.com>
2024-02-09 22:05:16 +0700

    created controller

---
+++ controller/todoController.js
@@ -0,0 +1,53 @@
+import { TodoModel } from "../model/todoModel.js";
+
+
+const todoController = {
+    getAll : async (req,res) => {
+        try { const todo = await TodoModel.find()
+            res.status(200).json(todo)
+            console.log("eiei");
+            
+        } catch (error) {
+            res.status(500).json(error)  
+        }
+    },
+    getById: async (req,res) => {
+        try { const todo = await TodoModel.findById(req.params.id)
+            res.status(200).json(todo)
+            
+        } catch (error) {
+            res.status(500).json(error)
+            
+        }
+    },
+    createTodo: async (req,res) => {
+        try {
+            const newTodo = await TodoModel.create(req.body)
+            res.status(201).json(newTodo)
+        } catch (error) {
+            res.status(500).json(error)
+            
+        }
+    },
+    updateTodo: async (req,res) => {
+        try {
+            const updateTodo = await TodoModel.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true})
+            res.status(200).json(updateTodo)
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    },
+    deleteTodo: async(req,res) => {
+        try {
+           await TodoModel.findOneAndDelete(req.params.id)
+           res.status(200).json("delete success")
+            
+        } catch (error) {
+            res.status(500).json(error)
+            
+        }
+    }
+
+}
+
+export {todoController}
